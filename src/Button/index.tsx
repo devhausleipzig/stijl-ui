6eed19e5dd002037c91afdb336cb9e29957a120d
Dan McAtee <danielmcatee@me.com>
2022-04-08 13:57:22 +0200

    add radix color and stitches config

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,11 +1,14 @@
-import { styled } from "@stitches/react";
+import { styled } from "../stitches.config";
 
 export const Button = styled("button", {
-  backgroundColor: "gainsboro",
+  appearance: "none",
+  border: "none",
+  backgroundColor: "$gray400",
   borderRadius: "9999px",
   fontSize: "13px",
-  padding: "10px 15px",
+  paddingX: "15px",
+  paddingY: "10px",
   "&:hover": {
-    backgroundColor: "lightgray",
+    backgroundColor: "$gray500",
   },
 });
---
+++ src/stitches.config.ts
@@ -0,0 +1,28 @@
+import { createStitches } from "@stitches/react";
+import { gray } from "@radix-ui/colors";
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    colors: {
+      gray400: gray.gray5,
+      gray500: gray.gray7,
+    },
+  },
+  media: {
+    bp1: "(min-width: 480px)",
+  },
+  utils: {
+    marginX: (value: string) => ({ marginLeft: value, marginRight: value }),
+    paddingX: (value: string) => ({ paddingLeft: value, paddingRight: value }),
+    paddingY: (value: string) => ({ paddingTop: value, paddingBottom: value }),
+  },
+});
